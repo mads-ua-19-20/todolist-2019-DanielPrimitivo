9907109dd38840fa8cf0ffce951767955477b749
Daniel Primitivo <dpc32@alu.ua.es>
2019-11-02 18:52:37 +0100

    Equipo con JPA, atributo id y repository para que se pueda grabar equipos

--- src/main/java/madstodolist/model/Equipo.java
+++ src/main/java/madstodolist/model/Equipo.java
@@ -1,9 +1,23 @@
 package madstodolist.model;
 
-public class Equipo {
+import javax.persistence.*;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "equipos")
+public class Equipo implements Serializable {
+
+    private static final long seerialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nombre;
 
     public Equipo(String nombre) { this.nombre = nombre; }
+
+    public Long getId() { return id; }
 
     public String getNombre() { return nombre; }
 }
