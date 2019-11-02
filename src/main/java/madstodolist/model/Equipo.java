f8906467952364993514df24f6c4c6da612c56f5
Daniel Primitivo <dpc32@alu.ua.es>
2019-11-02 18:32:09 +0100

    Clase Equipo añadida con atributo nombre y test comprobando su creación y obtención

---
+++ src/main/java/madstodolist/model/Equipo.java
@@ -0,0 +1,9 @@
+package madstodolist.model;
+
+public class Equipo {
+    private String nombre;
+
+    public Equipo(String nombre) { this.nombre = nombre; }
+
+    public String getNombre() { return nombre; }
+}
