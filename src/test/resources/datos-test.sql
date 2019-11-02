79eb03535078b799bf3ba265867b8af62f3ef33e
Daniel Primitivo <dpc32@alu.ua.es>
2019-11-02 22:01:23 +0100

    Relación mucho a muchos en base de datos y definición tabla equipo_usuario

--- src/test/resources/datos-test.sql
+++ src/test/resources/datos-test.sql
@@ -5,3 +5,4 @@ INSERT INTO tareas (id, titulo, usuario_id) VALUES('2', 'Renovar DNI', '1');
 INSERT INTO usuarios (id, email, nombre, password, fecha_nacimiento, tipo) VALUES('2', 'juan.lopez@gmail.com', 'Juan López', '12345678', '2001-02-10', 'admin');
 INSERT INTO usuarios (id, email, nombre, password, fecha_nacimiento, tipo) VALUES('3', 'paco.perez@gmail.com', 'Paco Pérez', '12345678', '2001-02-10', 'usuario');
 INSERT INTO equipos (id, nombre) VALUES('1', 'Proyecto Cobalto');
+INSERT INTO equipo_usuario (fk_equipo, fk_usuario) VALUES('1', '1');
