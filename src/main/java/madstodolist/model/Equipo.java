66ffb35257e67c95408e3264866a7a7d7b9ca8dc
Daniel Primitivo <dpc32@alu.ua.es>
2019-11-02 19:43:54 +0100

    equals y hashcode de la clase Equipo y su test

--- src/main/java/madstodolist/model/Equipo.java
+++ src/main/java/madstodolist/model/Equipo.java
@@ -2,7 +2,9 @@ package madstodolist.model;
 
 import javax.persistence.*;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "equipos")
@@ -13,11 +15,32 @@ public class Equipo implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String nombre;
 
     public Equipo(String nombre) { this.nombre = nombre; }
 
     public Long getId() { return id; }
 
+    public void setId(Long id) { this.id = id; }
+
     public String getNombre() { return nombre; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Equipo equipo = (Equipo) o;
+        if (id != null && equipo.id != null)
+            // Si tenemos los ID, comparamos por ID
+            return Objects.equals(id, equipo.id);
+        // sino comparamos por campos obligatorios
+        return nombre.equals(equipo.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        // Generamos un hash basado en los campos obligatorios
+        return Objects.hash(nombre);
+    }
 }
