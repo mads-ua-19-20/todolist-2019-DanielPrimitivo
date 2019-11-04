48bb3ef16a610c9eb4837f6d139e553c283e1d16
Daniel Primitivo <dpc32@alu.ua.es>
2019-11-04 20:21:04 +0100

    Listar usuarios de un equipo controller y vista

--- src/main/java/madstodolist/controller/EquipoController.java
+++ src/main/java/madstodolist/controller/EquipoController.java
@@ -1,6 +1,7 @@
 package madstodolist.controller;
 
 import madstodolist.authentication.ManagerUserSesion;
+import madstodolist.controller.exception.EquipoNotFoundException;
 import madstodolist.controller.exception.UsuarioNotFoundException;
 import madstodolist.model.Equipo;
 import madstodolist.model.Usuario;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -41,5 +43,27 @@ public class EquipoController {
         model.addAttribute("usuario", usuario);
         model.addAttribute("equipos", equipos);
         return "listaEquipos";
+    }
+
+    @GetMapping("/equipos/{id}/usuarios")
+    public String listadoUsuarios(@PathVariable(value="id") Long equipoId, Model model, HttpSession session) {
+        managerUserSesion.comprobarUsuarioLogeadoSession(session);
+        Long idUsuarioLogeado = (Long) session.getAttribute("idUsuarioLogeado");
+
+        Usuario usuario = usuarioService.findById(idUsuarioLogeado);
+        if (usuario == null) {
+            throw new UsuarioNotFoundException();
+        }
+
+        Equipo equipo = equipoService.findById(equipoId);
+        if (equipo == null) {
+            throw new EquipoNotFoundException();
+        }
+
+        List<Usuario> listaUsuarios = equipoService.usuariosEquipo(equipoId);
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("equipo", equipo);
+        model.addAttribute("listaUsuarios", listaUsuarios);
+        return "equipoListaUsuarios";
     }
 }
