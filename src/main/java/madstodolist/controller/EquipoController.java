9a86a3c25d1af261dcbca29e95cd34d9b20a34f1
Daniel Primitivo <dpc32@alu.ua.es>
2019-11-05 02:48:02 +0100

    Crear equipo controller y vista

--- src/main/java/madstodolist/controller/EquipoController.java
+++ src/main/java/madstodolist/controller/EquipoController.java
@@ -10,8 +10,8 @@ import madstodolist.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -27,6 +27,37 @@ public class EquipoController {
 
     @Autowired
     ManagerUserSesion managerUserSesion;
+
+    @GetMapping("/equipos/nuevo")
+    public String formNuevoEquipo(@ModelAttribute EquipoData equipoData, Model model,
+                                  HttpSession session) {
+        managerUserSesion.comprobarUsuarioLogeadoSession(session);
+        Long idUsuarioLogeado = (Long) session.getAttribute("idUsuarioLogeado");
+
+        Usuario usuario = usuarioService.findById(idUsuarioLogeado);
+        if (usuario == null) {
+            throw new UsuarioNotFoundException();
+        }
+
+        model.addAttribute("usuario", usuario);
+        return "formNuevoEquipo";
+    }
+
+    @PostMapping("/equipos/nuevo")
+    public String formNuevoEquipo(@ModelAttribute EquipoData equipoData, Model model,
+                                  RedirectAttributes flash, HttpSession session) {
+        managerUserSesion.comprobarUsuarioLogeadoSession(session);
+        Long idUsuarioLogeado = (Long) session.getAttribute("idUsuarioLogeado");
+
+        Usuario usuario = usuarioService.findById(idUsuarioLogeado);
+        if (usuario == null) {
+            throw new UsuarioNotFoundException();
+        }
+
+        equipoService.nuevoEquipo(equipoData.getNombre());
+        flash.addFlashAttribute("mensaje", "Equipo creado correctamente");
+        return "redirect:/equipos";
+    }
 
     @GetMapping("/equipos")
     public String listadoEquipos(Model model, HttpSession session) {
